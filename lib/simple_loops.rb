0ab7fc08a1f3aa4a3f46e6b6b31ba2ff1e1d7787
Colin Mosley <colin.a.mosley@gmail.com>
2020-06-25 20:24:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,6 +12,12 @@ def loop_message_n_times(string, integer)
   end 
 end 
 
-def output_array
+def output_array(array)
+  counter = 0 
+  while array[counter] do 
+    puts array[counter]
+    counter += 1 
+  end 
+end 
 
 def return_string_array
