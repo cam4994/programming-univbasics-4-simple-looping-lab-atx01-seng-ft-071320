b60ee1bef1449e40e7820badbefdcc46c93485b0
Colin Mosley <colin.a.mosley@gmail.com>
2020-06-25 20:28:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -20,4 +20,12 @@ def output_array(array)
   end 
 end 
 
-def return_string_array
+def return_string_array(array)
+  counter = 0 
+  while array[counter] do 
+    array[counter] = array[counter].to_s 
+    counter +=1 
+  end 
+  array 
+end 
+  
